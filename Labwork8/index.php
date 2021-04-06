7c9156e943658c39cf00a4a7a6d108b7a47560a2
myrongreensr <79763879+myrongreensr@users.noreply.github.com>
2021-04-05 19:19:03 -0500

    Add files via upload

---
+++ Labwork8/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+<title>Myron Green Table Practice</title>
+<meta charset="utf-8">
+<style>
+table { margin: auto;
+        border: 5px solid #000066;
+        width: 600px; 
+		border-spacing: 0;
+}
+td, th { padding: 0.5em;
+		 border-style: none;
+         font-family: Arial, sans-serif; 
+}
+caption { font-family: Verdana, sans-serif;
+          font-weight: bold;
+          font-size: 1.2em;
+          padding-bottom: 0.5em; 
+}
+tr:nth-of-type(even) { background-color: #eaeaea; }
+tr:first-of-type { background-color: #006;
+				   color: #eaeaea; }
+</style>
+</head>
+<body>
+ <table border="1">  
+ <caption>Dallas Baptist University Student Information</caption>
+   <tr>
+     <th>Student Id:</th>
+	 <th>City</th>
+     <th>State</th>
+     <th>Age</th>
+  </tr>
+  <tr>
+     <td>97064875</td>
+     <td>Dallas </td>
+	 <td>Texas</td>
+   <td>35</td>
+  </tr>
+  <tr>
+     <td>7528798</td>
+     <td>Kansas City</td>
+	 <td>KS</td>
+   <td>25</td>
+   </tr>
+    <tr>
+     <td>75863258</td>
+     <td>Washington</td>
+	 <td>DC</td>
+   <td>19</td>
+  </tr>
+</table>
+
+</body>
+</html>
